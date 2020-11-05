1cb8e4a85d88ce7860bd4bb4fbfbc552ead761b6
neethu <neethujose541@gmaial.com>
2020-11-05 20:30:06 +0530

    adding multiplication method in first file

--- firstone.sh
+++ firstone.sh
@@ -1,3 +1,4 @@
+
 echo "This is the firstone"
 echo "This is the first initialization"
 echo "Enter your number to calculate"
@@ -8,4 +9,7 @@ echo "The sum is" $sum
 echo "Lets substract two numbers"
 ((subResult=x-y))
 echo $subResult
+echo "Lets Multiply"
+((MultResult = x*y))
+echo $multResult
 
